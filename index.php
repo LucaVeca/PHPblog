0740c1ecbd3c7d83d5b989590fe915efecaff2f4
Luca Veca <lucaveca@gmail.com>
2014-10-29 10:42:24 -0700

    variables
    if, else with variables

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
 	</head>
 	<body>
 		<?php
-		//$myName = 'Hi my name is Luca';
-		//$Name = 'bob';
-		//echo $myName;
-		//if($Name === 'Luca'){
-			//echo 'i know you';
-		//}
-		//else{
-			//echo ' i dont know you';
-		//}
-		//echo"web server is running localy";
-		
+		$myName = 'Hi my name is Luca';
+		$Name = 'bob';
+		echo $myName;
+		if($Name === 'Luca'){
+			echo 'i know you';
+		}
+		else{
+			echo ' i dont know you ';
+		}
+		echo"web server is running localy";
+
 		//links to crete-db.php
 		require_once(__DIR__ . "/controller/create-db.php");
 		?>
