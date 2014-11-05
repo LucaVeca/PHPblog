a29713c6171252676a32cac5a8375ff90204a9bd
Luca Veca <lucaveca@gmail.com>
2014-11-05 11:05:59 -0800

    Posts
    users can input a title and a post

---
+++ view/form.php
@@ -0,0 +1,17 @@
+<h1>Create a Blog Post</h1>
+
+<form>
+	<div>
+		<!--Title-->
+		<label for="title">Title: </label>
+		<!--on one line-->
+		<input type="text" name="title" />
+	</div>
+
+	<div>
+		<!--Post-->
+		<label for="post">Post: </label>
+		<!--allows user to type more with multiple lines-->
+		<textarea name="post"></textarea>
+	</div>
+</form>
--- view/header.php
+++ view/header.php
@@ -4,8 +4,8 @@
 		<meta chartset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-		<link  type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
-		<link type="text/css" rel="stylesheet"  href="main.css">
+		<link rel="stylesheet" type="text/css"  href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css"  href="css/main.css">
 		<title></title>
 	</head>
 	<body>
