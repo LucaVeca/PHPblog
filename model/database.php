cbb51d10a3294261cd8ec2fb7e7736da42969d82
Luca Veca <lucaveca@gmail.com>
2014-11-14 11:03:43 -0800

    Class Comments

--- model/database.php
+++ model/database.php
@@ -1,5 +1,10 @@
 <?php
 //helps organize code 
+//moved classes here to help organize code and avoid repeating things
+//classes are used to store variables known as properties, atributes or fields 
+//defined by using one of the keywords public, protected, or private, followed by a normal variable declaration
+//A class is useful for containing the code and the relevant data together, isolated from everything else
+//classes are created because they can be used or called anywhere in your webpage and create an object
 class Database{
 	private $connection;
 	private $host;
