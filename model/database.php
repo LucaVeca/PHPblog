bc53fe7064ccd258348c6df3590178134d3a9115
Luca Veca <lucaveca@gmail.com>
2014-11-14 11:03:07 -0800

    New functions
    created public functions

--- model/database.php
+++ model/database.php
@@ -6,4 +6,24 @@ class Database{
 	private $username;
 	private $password;
 	private $database;
+
+	//establishes connection to global variables
+	public function__ construct($host, $username, $password, $database){
+		$this->host = $host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+	}
+
+	public function openConnection(){
+
+	}
+
+	public function closeConnection(){
+		
+	}
+
+	public function query($string){
+		
+	}
 }
