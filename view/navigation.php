c669ac326eab84988c24647e0361ad4e83340ba7
Luca Veca <lucaveca@gmail.com>
2014-12-16 21:49:03 -0800

    Protected more files

--- view/navigation.php
+++ view/navigation.php
@@ -1,6 +1,13 @@
 <?php 
 	//requires config from model file
 	require_once(__DIR__ . "/../model/config.php");
+	require_once(__DIR__ . "/../controller/login-verify.php");
+
+	//checking whether or not user has been logged in
+	if(!authenticateUser()){
+		header("Location: " . $path . "index.php");
+		die();
+	}
 ?>
 <nav>
 	<ul>
